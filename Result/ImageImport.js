50a590baac1470f883dd8917133ad5e36ed78678
siu-62 <6323127@ed.tus.ac.jp>
2025-10-22 16:40:27 +0900

    変更

--- Result/ImageImport.js
+++ Result/ImageImport.js
@@ -7,12 +7,13 @@ function ImageImport(files){
 
 
             //描画箇所に保存した画像を描画する
-            const ImageSpace = document.getElementById('ImageSpace');
-            const context = ImageSpace.getContext('2d');                //2D描画用のコンテキストを取得
+            const canvas = document.getElementById('ImageSpace');
+            const context = canvas.getContext('2d');                //2D描画用のコンテキストを取得
             const Img = new Image();
             Img.src = "${event.target.result}";
             sessionStorage.setItem("Img", JSON.stringify(Img));         //画像を他の関数でも使えるよう保存しておく
             context.drawImage(Img, 50, 50, 500, 400);
+            console.log("deteru?");
         }
         reader.readAsDataURL(files);                                     //これに成功するとreader.onloadが動き出す
 	}
