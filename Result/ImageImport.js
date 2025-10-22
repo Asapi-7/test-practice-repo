b40cbe8f00de3069ef182638e872474d7481a56c
siu-62 <6323127@ed.tus.ac.jp>
2025-10-22 21:11:22 +0900

    無事画像出るようになったんで一旦保存

--- Result/ImageImport.js
+++ Result/ImageImport.js
@@ -1,20 +1,25 @@
 function ImageImport(files){
     if (files.length === 0) return;
+		const file = files[0];              //もらうデータは必ずファイル群になってるから、先頭だけ抜き出して画像のみにする
     const reader = new FileReader();
-    if (files.type.match("image.*")) {
+    if (file.type.match("image.*")) {
 		reader.onload = (event) => { 
             //web APIにデータを送って、返り値を受け取るようにする
 
 
             //描画箇所に保存した画像を描画する
-            const canvas = document.getElementById('ImageSpace');
+            const canvas = document.getElementById('ImageSpace');   //描画領域となるcanvasを指定
             const context = canvas.getContext('2d');                //2D描画用のコンテキストを取得
             const Img = new Image();
-            Img.src = "${event.target.result}";
-            sessionStorage.setItem("Img", JSON.stringify(Img));         //画像を他の関数でも使えるよう保存しておく
-            context.drawImage(Img, 50, 50, 500, 400);
-            console.log("deteru?");
+            Img.src = event.target.result;                          //画像読み込み開始
+
+            Img.onload = () => {
+              const scale = canvas.width/Img.width;
+              context.drawImage(Img, 0, 0, Img.width*scale, Img.height*scale);
+              console.log("deteru?");
+              sessionStorage.setItem("Img", JSON.stringify(event.target.result));         //画像を他の関数でも使えるよう保存しておく    
+            }
         }
-        reader.readAsDataURL(files);                                     //これに成功するとreader.onloadが動き出す
+        reader.readAsDataURL(file);                                     //これに成功するとreader.onloadが動き出す
 	}
 }
