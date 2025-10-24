637904faad12652e663d64835ac2d67e3917e2eb
siu-62 <6323127@ed.tus.ac.jp>
2025-10-24 16:29:29 +0900

    wa-

--- Result/EffectSelect.js
+++ Result/EffectSelect.js
@@ -1,17 +1,47 @@
 function EffectSelect(effectName){
     //effectNameを元に、選択された画像と位置を取得、描画
     //画像インポート時にバックエンドから返される内容に応じて、フロントで行う処理の内容が変動
+    async function requestEffect(userRequest){                //asyncは内部に非同期処理が存在することを表す
+        try{
+            const response = await fetch('/get_stamp_info',{ //awaitは処理が終わるまで待機をお願いする
+                method: 'POST',                           //これはPOSTメソッドです
+                headers: {                                //送るデータはこの形状です
+                    'Content-Type': 'application/json'
+                },
+                body: userRequest                         //実際に送るデータです
+            });
+            if(!response.ok){
+                throw new Error('返答が芳しくなかった');
+            }
+            const result = await response.json();
+            return result;
+        }catch(error){
+            console.error('送れなかったよ:',error.message);
+            return null
+            }
+        }
+
+    const userID = sessionStorage.getItem("ID");
+    const userRequest = {
+        upload_image_id: userID,
+        stamp_id: effectName
+    }
+    const response = requestEffect(userRequest);
+    console.log("エフェクト画像もらえた");
     
     const ImageSpace = document.getElementById('ImageSpace');
     const context = ImageSpace.getContext('2d');                //2D描画用のコンテキストを取得
     const Img = new Image();
     Img.src = JSON.parse(sessionStorage.getItem("Img"));
 
-    Img,Onload = () => {
+    Img.Onload = () => {
         context.clearRect(0,0,ImageSpace.clientWidth,ImageSpace.clientHeight);
         const scale = canvas.width/Img.width;
+        //const effectImg = response.
+        const effectX = response.x;
+        const effectY = response.y;
+        const effectScale = response.scale;
         context.drawImage(Img, 0, 0, Img.width*scale, Img.height*scale);
-        //context.drawImage(Img, 50, 50, 500, 400);エフェクトこれ
-        sessionStorage.setItem("select", JSON.stringify(effectName));
+        //context.drawImage(effectImg, effectX, effectY, effectImg.width*effectScale, effectImg.height*effectScale);エフェクトこれ
     }
 }
--- Result/ImageImport.js
+++ Result/ImageImport.js
@@ -1,48 +1,52 @@
 function ImageImport(files){
-    if (files.length === 0) return;
-		const file = files[0];              //もらうデータは必ずファイル群になってるから、先頭だけ抜き出して画像のみにする
-    const reader = new FileReader();
-    if (file.type.match("image.*")) {
-		reader.onload = (event) => { 
-            //web APIにデータを送って、返り値を受け取るようにする
-            async function sendUserImage(userImage){                //asyncは内部に非同期処理が存在することを表す
-              try{
-                const response = await fetch('/upload_and_detect',{ //awaitは処理が終わるまで待機をお願いする
-                  method: 'POST',                           //これはPOSTメソッドです
-                  headers: {                                //送るデータはこの形状です
-                    'Content-Type': 'multipart/form-data'
-                  },
-                  file: userImage                           //実際に送るデータです
-                });
-                if(!response.ok){
-                  throw new Error('返答が芳しくなかった');
-                }
-                const result = await response.json();
-                return result;
-              }catch(error){
-                console.error('送れなかったよ:',error.message);
-                return null
-              }
-            }
-
-            const UserID = sendUserImage(file);
-            sessionStorage.setItem("ID", UserID);
-            console.log(UserID);
-
-
-            //描画箇所に保存した画像を描画する
-            const ImageSpace = document.getElementById('ImageSpace');   //描画領域となるcanvasを指定
-            const context = ImageSpace.getContext('2d');                //2D描画用のコンテキストを取得
-            const Img = new Image();
-            Img.src = event.target.result;                              //画像読み込み開始
-
-            Img.onload = () => {
-              const scale = ImageSpace.width/Img.width;
-              context.drawImage(Img, 0, 0, Img.width*scale, Img.height*scale);
-              console.log("deteru?");
-              sessionStorage.setItem("Img", JSON.stringify(event.target.result));         //画像を他の関数でも使えるよう保存しておく    
-            }
+  if (files.length === 0) return;
+	const file = files[0];              //もらうデータは必ずファイル群になってるから、先頭だけ抜き出して画像のみにする
+  const reader = new FileReader();
+  if (file.type.match("image.*")) {
+	  reader.onload = (event) => { 
+      //web APIにデータを送って、返り値を受け取るようにする
+      async function sendUserImage(newUserImage){                //asyncは内部に非同期処理が存在することを表す
+        try{
+          const response = await fetch('/upload_and_detect',{ //awaitは処理が終わるまで待機をお願いする
+            method: 'POST',                                   //これはPOSTメソッドです
+            headers: {                                        //送るデータはこの形状です
+              'Content-Type': 'multipart/form-data'
+            },
+            body: newUserImage                                   //実際に送るデータです
+          });
+          if(!response.ok){
+            throw new Error('返答が芳しくなかった');
+          }
+          const result = await response.json();
+          return result;
+        }catch(error){
+          console.error('送れなかったよ:',error.message);
+          return null
         }
-        reader.readAsDataURL(file);                                     //これに成功するとreader.onloadが動き出す
+      }
+    
+      const newUserImage = {
+        file: this.file
+      }
+      const response = sendUserImage(newUserImage);
+      const userID = response.session_id;
+      sessionStorage.setItem("ID", userID);
+      console.log(userID);
+
+
+      //描画箇所に保存した画像を描画する
+      const ImageSpace = document.getElementById('ImageSpace');   //描画領域となるcanvasを指定
+      const context = ImageSpace.getContext('2d');                //2D描画用のコンテキストを取得
+      const Img = new Image();
+      Img.src = event.target.result;                              //画像読み込み開始
+
+      Img.onload = () => {
+        const scale = ImageSpace.width/Img.width;
+        context.drawImage(Img, 0, 0, Img.width*scale, Img.height*scale);
+        console.log("deteru?");
+        sessionStorage.setItem("Img", JSON.stringify(event.target.result));         //画像を他の関数でも使えるよう保存しておく    
+      }
+    }
+    reader.readAsDataURL(file);                                     //これに成功するとreader.onloadが動き出す
 	}
 }
